b5a7a1a7649f51fde5e40f40eb8ef269486d541e
Georg Daniel Vahtramäe <48998862+GeorgDV@users.noreply.github.com>
2020-04-29 14:21:28 +0300

    Add scripts

--- bootstrap-script.sh
+++ bootstrap-script.sh
@@ -1,33 +1,26 @@
-# minecraft-server-docker
+#!/bin/sh
 
- * Git clone this repo
- * Install packages ( check [install-soft.sh](./install-soft.sh) )
- 
-
-## docker
-
-```
 sudo yum update -y
+
+# docker
+
 sudo yum install -y docker
 sudo usermod -a -G docker ec2-user
 sudo service docker start
-sudo docker run --rm -e EULA=TRUE -p 25565:25565 --name mc itzg/minecraft-server
-```
-## docker compose
 
-```
+# docker-compose
+
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-docker-compose --version
-```
-## git
+sudo docker-compose --version
 
-```
+# git 
+
 sudo yum install -y git
-```
 
-## after clone run 
-```
-sudo docker-compose up
-```
+# insatall/run minecraft 
+
+git clone https://github.com/eritikass/minecraft-server-docker.git
+cd minecraft-server-docker
+sudo docker-compose up -d
--- install-soft.sh
+++ install-soft.sh
@@ -7,15 +7,21 @@ sudo yum update -y
 sudo yum install -y docker
 sudo usermod -a -G docker ec2-user
 sudo service docker start
-sudo docker run --rm -e EULA=TRUE -p 25565:25565 --name mc itzg/minecraft-server
+# sudo docker run --rm -e EULA=TRUE -p 25565:25565 --name mc itzg/minecraft-server
 
 # docker-compose
 
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-docker-compose --version
+sudo docker-compose --version
 
 # git 
 
 sudo yum install -y git
+
+# insatall/run mincraft 
+
+git clone https://github.com/eritikass/minecraft-server-docker.git
+cd minecraft-server-docker
+sudo docker-compose up -d
