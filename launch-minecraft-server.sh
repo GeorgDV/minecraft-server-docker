1a7be7e2645eb39d97aae60fc27cf15dadd1f2d1
Georg Daniel Vahtramäe <48998862+GeorgDV@users.noreply.github.com>
2020-05-06 13:45:55 +0300

    Add script for running minecraft server

---
+++ launch-minecraft-server.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+SECURITY_GROUP_IDS="sg-0e9fa95aa92415afd"
+SUBNET_ID="subnet-2de1fb03"
+
+aws ec2 run-instances \
+ --image-id=ami-0915e09cc7ceee3ab \
+ --count=1 --instance-type=t2.medium \
+ --key-name=quarantine-key-pair \
+ --security-group-ids=$SECURITY_GROUP_IDS \
+ --subnet-id=$SUBNET_ID \
+ --user-data=file://bootstrap-script.sh
