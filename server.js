144e16a38b9bd5e3c463a8bdaa8e854a4610fc62
Georg Daniel Vahtramäe <48998862+GeorgDV@users.noreply.github.com>
2020-05-13 14:58:23 +0300

    Add 'button' for creating a server

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express')
-const addServer = require('./add-server')
+const addServer = require('./js/add-server')
 const app = express()
+const path = require('path');
 const port = 3000
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/add-server.html'));
+});
 
 app.get('/api/v1/add-server', (req, res) => {
     //THIS CREATES NEW VIRTUAL MACHINE AND SERVER
     //addServer();
-    
+
     addServer((data) => {
         console.log('data', data);
         res.json(data);
